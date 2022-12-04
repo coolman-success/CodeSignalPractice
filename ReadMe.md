a1ae1db246768a2b296ff714522f5b54e185e51b
coolman-success <coolman.success@gmail.com>
2022-12-04 15:34:17 +0800

    Added markdown formatting script and readme file, Arcade/Intro/1

---
+++ ReadMe.md
@@ -0,0 +1,13 @@
+# CodeSignalPractice
+
+This repo is my solution submits to [CodeSignal](https://codesignal.com).
+
+## About CodeSignal
+
+##### [From Wikipedia](https://en.wikipedia.org/wiki/CodeSignal)
+
+[CodeSignal](https://codesignal.com) is a technical interview and assessment platform operated by American company BrainFights, Inc.
+
+CodeSignal, initially called CodeFights, was founded in 2014. CodeFights provided a platform for developers to compete in head-to-head timed coding challenges. In 2017, CodeFights began to offer an interview practice mode, as well as a product for recruiters to connect with qualified technical candidates.
+
+In 2018, the company changed the name of its platform from CodeFights to CodeSignal, and introduced a Coding Score that developers could share with technical recruiters, based on challenges completed on CodeSignal.[8] Since then, the CodeSignal platform has expanded to offer a suite of technical interview and assessment products.
---
+++ mdconv.py
@@ -0,0 +1,39 @@
+import sys
+import re
+
+# get markdown file as parameter
+mdfile = [param for param in sys.argv if param[-3:] == ".md"]
+
+if len(mdfile) == 0:
+    print("Should be provided with the path to a markdown filename (.md)")
+    sys.exit()
+
+# read markdown file
+f = open(mdfile[0], "r")
+lines = f.readlines()
+f.close()
+
+# remove blank lines
+lines = [line for line in lines if line != "\n"]
+
+# reformatting
+f_param = False
+for i, line in enumerate(lines):
+    if line == "\n":
+        continue
+    elif line == "Example\n":
+        lines[i] = "\n## Example\n\n"
+    elif line == "Input/Output\n":
+        lines[i] = "\n## Input/Output\n\n"
+        f_param = True
+    elif re.match(r"^(\[input\]|\[output\]|\[execution time limit\])\s", line, re.IGNORECASE):
+        lines[i] = "- **" + line[:-1] + "**\n\n"
+    elif f_param:
+        lines[i] = "\t" + line + "\n"
+
+# write updated format markdown
+with open(mdfile[0], "w") as f:
+    for line in lines:
+        if line == "\t\n":
+            continue
+        f.write(line)
